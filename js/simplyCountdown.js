72fdd9d637877cce23822ee8679aa897d546ba34
sifuna <55757423+sifuna2002@users.noreply.github.com>
2020-11-10 12:50:37 +0300

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -1,14 +1,4 @@
-/*!
- * Project : simply-countdown
- * File : simplyCountdown
- * Date : 27/06/2015
- * License : MIT
- * Version : 1.3.2
- * Author : Vincent Loy <vincent.loy1@gmail.com>
- * Contributors : 
- *  - Justin Beasley <JustinB@harvest.org>
- *  - Nathan Smith <NathanS@harvest.org>
- */
+
 /*global window, document*/
 (function (exports) {
     'use strict';
@@ -115,9 +105,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2020,
+                month: 12,
+                day: 18,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
